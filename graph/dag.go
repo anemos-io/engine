f13b113e91bf3251189979a80f954cf26d29a499
Alex Van Boxel <alex@vanboxel.be>
2017-08-18 18:54:05 +0200

    Load graphs from file and create node graph

--- graph/dag.go
+++ graph/dag.go
@@ -1,14 +1,19 @@
 package graph
 
 import (
-	"fmt"
-	"github.com/ghodss/yaml"
+	//"fmt"
+	//"github.com/ghodss/yaml"
 	"io/ioutil"
+	"gopkg.in/yaml.v2"
+
+	"log"
+	"fmt"
+	"github.com/anemos-io/engine"
 )
 
 type TaskConfig struct {
-	Name string
-	TaskRef string
+	Name       string
+	TaskRef    string
 	Downstream []TaskConfig
 }
 
@@ -26,21 +31,125 @@ type DagConfig struct {
 	Tasks    []TaskConfig
 }
 
-func ParseDag(filename string) *DagConfig {
-	dat, err := ioutil.ReadFile(filename)
-	var dagConfig DagConfig
-	err = yaml.Unmarshal(dat, &dagConfig)
-	if err != nil {
-		fmt.Printf("err: %v\n", err)
-		return nil
+type builder struct {
+	nodes    map[string]anemos.Node
+	bindings map[string]binding
+}
+type binding struct {
+	name string
+	down string
+	up   string
+}
+
+func parseTask(builder *builder, config map[interface{}]interface{}, parent anemos.Node) (anemos.Node) {
+	name := config["name"].(string)
+
+	task := NewTaskNode()
+	builder.nodes[name] = task
+	task.Name = name
+
+	download, found := config["downstream"]
+	if found {
+		for _, v := range download.([]interface{}) {
+			t := v.(map[interface{}]interface{})
+			taskConfig, found := t["task"].(map[interface{}]interface{})
+			if found {
+				dt := parseTask(builder, taskConfig, task)
+				//fmt.Println(task)
+				bn := fmt.Sprintf("%s>%s", task.ShortName(), dt.ShortName())
+				builder.bindings[bn] = binding{
+					name: bn,
+					up:   task.ShortName(),
+					down: dt.ShortName(),
+				}
+			} else {
+				taskRef, found := t["taskRef"].(string)
+				if found {
+					bn := fmt.Sprintf("%s>%s", task.ShortName(), taskRef)
+					builder.bindings[bn] = binding{
+						name: bn,
+						up:   task.ShortName(),
+						down: taskRef,
+					}
+				}
+			}
+		}
 	}
-	return &dagConfig
+	return task
 
-	//fmt.Println(dagConfig.Tasks[0].Name)
-	//fmt.Println(dagConfig.Tasks[0].Body) //["reference"])
+}
+
+func ParseDag(filename string) *Group {
+	data, err := ioutil.ReadFile(filename)
+	dagConfig := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal([]byte(data), &dagConfig)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	builder := &builder{
+		nodes:    make(map[string]anemos.Node),
+		bindings: make(map[string]binding),
+	}
+
+	metaData := dagConfig["metaData"].(map[interface{}]interface{})
+
 	//
-	//for k, v := range dagConfig.Tasks[0].Body {
-	//	fmt.Printf("key[%s] value[%s]\n", k, v)
-	//}
+	group := NewGroup()
+	group.Name = metaData["name"].(string)
 
+	tasksConfig := dagConfig["tasks"].([]interface{})
+	fmt.Println(tasksConfig)
+	for _, v := range tasksConfig {
+		parseTask(builder, v.(map[interface{}]interface{}), nil)
+	}
+
+	c, found := dagConfig["bindings"]
+	if found {
+		bindingsConfig := c.([]interface{})
+		fmt.Println(bindingsConfig)
+		for _, v := range bindingsConfig {
+			bindingConfig := v.(map[interface{}]interface{})
+			n1 := bindingConfig["taskRef"].(string)
+			d, found := bindingConfig["downstream"]
+			if found {
+				downstream := d.([]interface{})
+				for _, d := range downstream {
+					n2 := d.(map[interface{}]interface{})["taskRef"].(string)
+					bn := fmt.Sprintf("%s>%s", n1, n2)
+					builder.bindings[bn] = binding{
+						name: bn,
+						up:   n1,
+						down: n2,
+					}
+				}
+			}
+			u, found := bindingConfig["upstream"]
+			if found {
+				upstream := u.([]interface{})
+				for _, u := range upstream {
+					n2 := u.(map[interface{}]interface{})["taskRef"].(string)
+					bn := fmt.Sprintf("%s>%s", n2, n1)
+					builder.bindings[bn] = binding{
+						name: bn,
+						up:   n2,
+						down: n1,
+					}
+				}
+			}
+
+		}
+	}
+
+
+	for _, v := range builder.nodes {
+		group.AddNode(v)
+	}
+	for _, v := range builder.bindings {
+		LinkDownNamed(builder.nodes[v.up], builder.nodes[v.down], v.name)
+	}
+	group.Resolve()
+
+	return group
 }
--- graph/dag_test.go
+++ graph/dag_test.go
@@ -5,16 +5,34 @@ import (
 	//"fmt"
 	//"sync"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
-func TestParseDag_ThreeTaskSimple(t *testing.T) {
-	dagConfig := ParseDag("three-task-simple.yaml")
+func Equavalent_Dags(name string, t *testing.T) {
+	group := ParseDag(fmt.Sprintf("%s.yaml",name))
 
-	assert.Equal(t, "anemos/dag", dagConfig.Kind)
-	assert.Equal(t, "v1", dagConfig.Version)
-	assert.Equal(t, "three-node-simple", dagConfig.MetaData.Name)
-	assert.Equal(t, "task-begin", dagConfig.Tasks[0].Name)
-	assert.Equal(t, "task-top", dagConfig.Tasks[0].Downstream[0].Name)
-	assert.Equal(t, "task-bottom", dagConfig.Tasks[0].Downstream[1].Name)
+	assert.Equal(t, 1, len(group.begin.downstream))
+	assert.Equal(t, 2, len(group.end.upstream))
+
+	taskStart := group.begin.downstream[fmt.Sprintf("%s+begin>task-start",name)]
+	assert.Equal(t, "task-start", taskStart.ShortName())
+	assert.Contains(t, taskStart.Downstream(),"task-start>task-left")
+	assert.Equal(t, "task-left", taskStart.Downstream()["task-start>task-left"].ShortName())
+	assert.Contains(t, taskStart.Downstream(),"task-start>task-right")
+	assert.Equal(t, "task-right", taskStart.Downstream()["task-start>task-right"].ShortName())
 }
+
+func TestParseDag_ThreeTaskSimple(t *testing.T) {
+	Equavalent_Dags("three-task-simple", t)
+}
+
+
+func TestParseDag_ThreeTaskFlat(t *testing.T) {
+	Equavalent_Dags("three-task-flat", t)
+}
+
+func TestParseDag_ThreeTaskBinding(t *testing.T) {
+	Equavalent_Dags("three-task-binding", t)
+}
+
 
